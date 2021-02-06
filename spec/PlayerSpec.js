cd94cd8c7a07646b1022d84d65c5a415717cf111
CodeKrakken <newholm1@hotmail.com>
2021-02-06 17:00:54 +0000

    Cannot play without money in wallet

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,3 +1,22 @@
 'use strict'
 
 const Player = require('../lib/Player')
+let player
+
+describe('player', function() {
+  beforeEach(function() {
+    player = new Player
+  })
+
+  it('has a wallet', function() {
+    expect(player.wallet).toBeDefined()
+  })
+
+  it('can play the machine', function() {
+    expect(player.play).toBeDefined()
+  })
+
+  it('cannot play the machine with insufficient money', function() {
+    expect(player.play()).toEqual('Insert Coin.')
+  })
+})
