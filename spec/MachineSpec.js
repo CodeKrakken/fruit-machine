5cac73accca9fd0bc45f5abf636ac241c6aa8ed9
CodeKrakken <newholm1@hotmail.com>
2021-02-06 15:40:36 +0000

    can be initialized

--- spec/MachineSpec.js
+++ spec/MachineSpec.js
@@ -1,2 +1,13 @@
 'use strict';
 const Machine = require('../lib/Machine')
+let machine
+
+describe('machine', function() {
+  beforeEach(function() {
+    machine = new Machine()
+  })
+
+  it('can be initialized', function() {
+    expect(machine).toBeDefined()
+  })
+})
