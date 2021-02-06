e668b05738cad719d28e302e0aa27ade720627cc
CodeKrakken <newholm1@hotmail.com>
2021-02-06 15:46:42 +0000

    Add tests for specific colours

--- spec/MachineSpec.js
+++ spec/MachineSpec.js
@@ -11,7 +11,14 @@ describe('machine', function() {
     expect(machine).toBeDefined()
   })
 
-  it('has an array of colours', function() {
+  it('has an array for colours', function() {
     expect(machine.colours).toBeDefined()
+  })
+
+  it('has the right colours', function() {
+    expect(machine.colours[0]).toEqual('black')
+    expect(machine.colours[1]).toEqual('white')
+    expect(machine.colours[2]).toEqual('green')
+    expect(machine.colours[3]).toEqual('yellow')
   })
 })
