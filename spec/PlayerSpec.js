f5be5ebf63a3fb8c00cef7dfa7835b221d8a38df
CodeKrakken <newholm1@hotmail.com>
2021-02-07 12:36:04 +0000

    Refactor Player tests

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -11,7 +11,6 @@ describe('player', function() {
       float: 100
     }
     player = new Player(machine)
-    player.wallet = 1
   })
 
   it('has a wallet', function() {
@@ -26,58 +25,68 @@ describe('player', function() {
     expect(player.play).toBeDefined()
   })
 
-  it('can play a game', function() {
-    spyOn(machine, 'play')
-    player.play()
-    expect(machine.play).toHaveBeenCalled()
-  })
-
   it('cannot play the machine with insufficient money', function() {
-    player.wallet = 0
     expect(player.play()).toEqual('Insert Coin.')
   })
 
-  it('gets charged for playing', function() {
-    player.play()
-    expect(player.wallet).toEqual(0)
-    expect(machine.float).toEqual(101)
-  })
-
-  it('can win the jackpot', function() {
-    spyOn(machine, 'play').and.returnValue('Jackpot!')
-    player.play()
-    expect(machine.float).toEqual(0)
-    expect(player.wallet).toEqual(101)
-  })
-
-  it('can win the halfpot', function() {
-    spyOn(machine, 'play').and.returnValue('Halfpot!')
-    player.play()
-    expect(machine.float).toEqual(51)
-    expect(player.wallet).toEqual(50)
-  })
-
-  it('can win the quarterpot', function() {
-    spyOn(machine, 'play').and.returnValue('Quarterpot!')
-    player.play()
-    expect(player.wallet).toEqual(5)
-    expect(machine.float).toEqual(96)
-  })
-
-  it('gains play credits if machine float cannot cover winnings', function() {
-    spyOn(machine, 'play').and.returnValue('Quarterpot!')
-    machine.float = 3
-    player.play()
-    expect(player.credit).toEqual(1)
-    expect(machine.float).toEqual(0)
-  })
-
-  it('uses credits before money when playing the game', function() {
-    player.wallet = 0
+  it('can play the game with credits', function() {
     player.credit = 1
     player.play()
     expect(player.credit).toEqual(0)
   })
 
+  describe('with money in wallet', function() {
+    beforeEach(function() {
+      player.wallet = 1
+    })
+
+    it('can play a game', function() {
+      spyOn(machine, 'play')
+      player.play()
+      expect(machine.play).toHaveBeenCalled()
+    })
+  
+    it('gets charged for playing', function() {
+      player.play()
+      expect(player.wallet).toEqual(0)
+      expect(machine.float).toEqual(101)
+    })
+  
+    it('can win the jackpot', function() {
+      spyOn(machine, 'play').and.returnValue('Jackpot!')
+      player.play()
+      expect(machine.float).toEqual(0)
+      expect(player.wallet).toEqual(101)
+    })
+  
+    it('can win the halfpot', function() {
+      spyOn(machine, 'play').and.returnValue('Halfpot!')
+      player.play()
+      expect(machine.float).toEqual(51)
+      expect(player.wallet).toEqual(50)
+    })
+
+    describe('dealing with the quarterpot', function() {
+      beforeEach(function() {
+        spyOn(machine, 'play').and.returnValue('Quarterpot!')
+      })
+
+      it('can win the quarterpot', function() {
+        player.play()
+        expect(player.wallet).toEqual(5)
+        expect(machine.float).toEqual(96)
+      })
+    
+      it('gains play credits if machine float cannot cover winnings', function() {
+        machine.float = 3
+        player.play()
+        expect(player.credit).toEqual(1)
+        expect(machine.float).toEqual(0)
+      })
+
+    })
+  
+
+  })
 })
 
